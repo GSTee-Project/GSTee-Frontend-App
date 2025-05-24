4af661b23c7dff9157a4727db4388555b2bbac32
SamuelOmodele <samuelomodele19@gmail.com>
2025-05-24 09:57:59 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './styles/globals.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='app'>
+      App development 
+    </div>
+  )
+}
+
+export default App
