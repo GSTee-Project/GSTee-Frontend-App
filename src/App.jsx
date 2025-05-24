740595a9c9627e28f613c95181eb42674e131401
SamuelOmodele <samuelomodele19@gmail.com>
2025-05-24 15:49:23 +0100

    tree-shaking components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './styles/globals.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='app'>
-      App development 
+      Gstee Application
     </div>
   )
 }
