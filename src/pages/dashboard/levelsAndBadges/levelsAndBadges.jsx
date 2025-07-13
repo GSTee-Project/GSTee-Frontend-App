516086074ac1841dce70e72aa8745ec9371a3582
Rainbooboo <omoneferuemu@gmail.com>
2025-07-13 18:16:56 +0100

    levels and badges mobile view

--- src/pages/dashboard/levelsAndBadges/levelsAndBadges.jsx
+++ src/pages/dashboard/levelsAndBadges/levelsAndBadges.jsx
@@ -18,7 +18,7 @@ const LevelsAndBadges = () => {
       <div className={styles.statIcon}>{icon}</div>
       <div className={styles.statText}>
         <p className={styles.statValue}>{value}</p>
-        <p>{label}</p>
+        <p className={styles.statLabel}>{label}</p>
       </div>
     </div>
   );
@@ -31,7 +31,7 @@ const LevelsAndBadges = () => {
     { title: "Speed Demon", icon: <SlSpeedometer />, earned: "Feb 5,2025", locked: false },
     { title: "Boss Slayer", icon: <RiSwordLine />, earned: "March 5,2025", locked: false },
     {
-      title: "Gold Streak", icon: <svg width="86" height="90" viewBox="0 0 86 90" fill="none" xmlns="http://www.w3.org/2000/svg">
+      title: "Gold Streak", icon: <svg className={styles.barIcon} viewBox="0 0 86 90" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M42.583 3V11.3333M23.833 9.25L30.083 15.5M61.333 9.25L55.083 15.5M52.0205 71.2167C53.3372 67.1958 53.9955 65.1875 55.3414 63.8042C55.983 63.1431 56.715 62.6069 57.5372 62.1958C59.2497 61.3333 61.333 61.3333 65.4997 61.3333C69.6664 61.3333 71.7497 61.3333 73.4664 62.1958C74.283 62.6069 75.015 63.1431 75.6622 63.8042C77.008 65.1875 77.6622 67.1958 78.983 71.2167L80.3414 75.3625C81.983 80.3708 82.8039 82.8708 81.5789 84.6042C80.3497 86.3333 77.758 86.3333 72.5705 86.3333H58.433C53.2455 86.3333 50.6497 86.3333 49.4247 84.6042C48.1955 82.8708 49.0164 80.3708 50.658 75.3625L52.0205 71.2167ZM29.0997 37.8833C30.4164 33.8625 31.0789 31.8542 32.4205 30.4708C33.0677 29.8097 33.7997 29.2736 34.6164 28.8625C36.333 28 38.4164 28 42.583 28C46.7497 28 48.833 28 50.5497 28.8625C51.3664 29.2736 52.0983 29.8097 52.7455 30.4708C54.0872 31.8542 54.7455 33.8625 56.0664 37.8833L57.4247 42.0292C59.0664 47.0375 59.883 49.5375 58.6622 51.2708C57.433 53 54.8414 53 49.6497 53H35.5164C30.3247 53 27.733 53 26.508 51.2708C25.2789 49.5375 26.0997 47.0375 27.7414 42.0292L29.0997 37.8833ZM6.18302 71.2167C7.50386 67.1958 8.16219 65.1875 9.50802 63.8042C10.1497 63.1431 10.8816 62.6069 11.7039 62.1958C13.4164 61.3333 15.4997 61.3333 19.6664 61.3333C23.833 61.3333 25.9164 61.3333 27.633 62.1958C28.4497 62.6069 29.1816 63.1431 29.8289 63.8042C31.1747 65.1875 31.8289 67.1958 33.1497 71.2167L34.508 75.3625C36.1497 80.3708 36.9705 82.8708 35.7455 84.6042C34.5164 86.3333 31.9247 86.3333 26.7372 86.3333H12.5997C7.41219 86.3333 4.81636 86.3333 3.59136 84.6042C2.36219 82.8708 3.18303 80.3708 4.82469 75.3625L6.18302 71.2167Z" stroke="black" strokeWidth="6" strokeLinecap="round" strokeLinejoin="round" />
       </svg>, earned: "April 10,2025", locked: false
     },
@@ -61,7 +61,7 @@ const LevelsAndBadges = () => {
           <div className={styles.levelDiv1}>
             <p className={styles.level}>Level 4</p>
             <p className={styles.xp}>700 XP</p>
-            <LinearProgressBar value={60} />
+            <LinearProgressBar value={60}  />
             <p className={styles.nextLevelXp}>Next Level: <span>900 XP</span></p>
           </div>
         </div>
